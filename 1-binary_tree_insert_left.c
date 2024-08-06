274ec4441071c49a018251fe968e4d094b5b1676
kimikoultramega <9379@holbertonstudents.com>
2024-08-06 20:13:56 +0000

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,37 +8,33 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *aux = parent->left;
-	binary_tree_t *new_node;
+    binary_tree_t *new_node, *aux;
 
-	new_node = malloc(sizeof(binary_tree_t));
+    if (parent == NULL)
+    {
+    	return (NULL);
+    }
 
-	if (parent == NULL)
-	{
-		return (NULL);
-	}
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
-	else
-	{
-		new_node->n = value;
-		new_node->left = NULL;
-		new_node->right = NULL;
-	}
+    new_node = malloc(sizeof(binary_tree_t));
 
-	if (parent->left != NULL)
-	{
-		free(aux);
-		parent->right = NULL;
-		new_node->left = aux;
-	}
-	else
-	{
-		parent->left = new_node;
-		parent->right = NULL;
-	}
+    if (new_node == NULL)
+    {
+        return (NULL);
+    }
 
-	return (new_node);
+        new_node->n = value;
+	new_node->parent = parent;
+        new_node->left = NULL;
+        new_node->right = NULL;
+
+    if (parent->left != NULL)
+    {
+	aux = parent->left;
+        parent->left = new_node;
+	aux->parent = new_node;
+	new_node->left = aux;
+    }
+    parent->left = new_node;
+
+    return (new_node);
 }
