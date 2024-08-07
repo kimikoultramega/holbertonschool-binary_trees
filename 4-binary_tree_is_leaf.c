7cd37537687238ade6592e25db82d49c0a025d91
kimikoultramega <9379@holbertonstudents.com>
2024-08-07 15:16:58 +0000

    4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -15,10 +15,10 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	}
 	if (node->left == NULL && node->right == NULL)
 	{
-		return (0);
+		return (1);
 	}
 	else
 	{
-		return (1);
+		return (0);
 	}
 }
