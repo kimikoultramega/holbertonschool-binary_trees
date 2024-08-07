bb91dc35eb59a827f66cd5768e377fd04932a5b7
kimikoultramega <9379@holbertonstudents.com>
2024-08-07 16:24:16 +0000

    5-binary_tree_is_root.c

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - lista
+ * @node: node
+ * Return: 0 o 1
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+	if (node->parent == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+
+}
+
