2d1c177ae3636808cdabf922b1b8db2e9ef345fa
root <julietabobadilla123@gmail.com>
2024-08-07 18:04:21 +0000

    6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -3,6 +3,7 @@
 /**
  * binary_tree_preorder - pre order traversal
  * @tree: pointer to the root node
+ * @func: pointer to a function
  * Return: Always 0
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
